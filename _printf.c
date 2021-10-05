68429653a7694d448653b5e0ef50a819732da0dc
der-bew <derbew12@gmail.com>
2021-10-05 19:42:41 +0300

    Update: _printf.c file and file _putchar.c

--- _printf.c
+++ _printf.c
@@ -1,33 +1,6 @@
 #include "main.h"
 
-/**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
- */
-static int (*check_for_specifiers(const char *format))(va_list)
-{
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c}, {"s", print_s},
-		{"i", print_i}, {"d", print_d},
-		{"u", print_u}, {"b", print_b},
-		{"o", print_o}, {"x", print_x},
-		{"X", print_X}, {"p", print_p},
-		{"S", print_S}, {"r", print_r},
-		{"R", print_R}, {NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
+static int (*check_for_specifiers(const char *format))(va_list);
 
 /**
  * _printf - Receives the main string and all the necessary parameters to
@@ -80,3 +53,39 @@ int _printf(const char *format, ...)
 
 	return (count);
 }
+
+/**
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}chars.c hex.c num.c rot13.c un_int.c
