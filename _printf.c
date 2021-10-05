76384843d2a16aff94e59fc00dc474ac8820eb2c
der-bew <derbew12@gmail.com>
2021-10-05 19:54:40 +0300

    Fix error: _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-static int (*check_for_specifiers(const char *format))(va_list);
+static int (*check_spec(const char *format))(va_list);
 
 /**
  * _printf - Receives the main string and all the necessary parameters to
@@ -33,7 +33,7 @@ int _printf(const char *format, ...)
 		}
 		if (!format[i])
 			return (count);
-		f = check_for_specifiers(&format[i + 1]);
+		f = check_spec(&format[i + 1]);
 		if (f != NULL)
 		{
 			count += f(valist);
@@ -55,29 +55,22 @@ int _printf(const char *format, ...)
 }
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
+ * check_spec - checks if there is a valid format specifier
  * @format: possible format specifier
  *
  * Return: pointer to valid function or NULL
  */
-static int (*check_for_specifiers(const char *format))(va_list)
+static int (*check_spec(const char *format))(va_list)
 {
 	unsigned int i;
 	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
+		{"c", print_c}, {"s", print_s},
+		{"i", print_i}, {"d", print_d},
+		{"u", print_u}, {"b", print_b},
+		{"o", print_o}, {"x", print_x},
+		{"X", print_X}, {"p", print_p},
+		{"S", print_S}, {"r", print_r},
+		{"R", print_R}, {NULL, NULL}
 	};
 
 	for (i = 0; p[i].t != NULL; i++)
@@ -88,4 +81,4 @@ static int (*check_for_specifiers(const char *format))(va_list)
 		}
 	}
 	return (p[i].f);
-}chars.c hex.c num.c rot13.c un_int.c
+}
