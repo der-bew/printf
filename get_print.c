d31e71b794c4c9ac69783e3c33b6cf06ee3377ba
der-bew <derbew12@gmail.com>
2021-10-08 01:52:59 +0300

    Fix errors

--- get_print.c
+++ get_print.c
@@ -4,7 +4,6 @@
  * get_print - selects the right printing function
  * depending on the conversion specifier passed to _printf
  * @s: character that holds the conversion specifier
- * Description: the function loops through the structs array
  * func_arr[] to find a match between the specifier passed to _printf
  * and the first element of the struct, and then the approriate
  * printing function
@@ -13,20 +12,13 @@
 int (*get_print(char s))(va_list, flags_t *)
 {
 	ph func_arr[] = {
-		{'i', print_int},
-		{'s', print_string},
-		{'c', print_char},
-		{'d', print_int},
-		{'u', print_unsigned},
-		{'x', print_hex},
-		{'X', print_hex_big},
-		{'b', print_binary},
-		{'o', print_octal},
-		{'R', print_rot13},
-		{'r', print_rev},
-		{'S', print_bigS},
-		{'p', print_address},
-		{'%', print_percent}
+		{'i', print_int}, {'s', print_string},
+		{'c', print_char}, {'d', print_int},
+		{'u', print_unsigned}, {'x', print_hex},
+		{'X', print_hex_big}, {'b', print_binary},
+		{'o', print_octal}, {'R', print_rot13},
+		{'r', print_rev}, {'S', print_bigS},
+		{'p', print_address}, {'%', print_percent}
 		};
 	int flags = 14;
 
